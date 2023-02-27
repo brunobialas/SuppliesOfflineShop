f0938a7800d5c10b9cd224f90affd12b1e4e1e9d
brunobialas <zehuwancode@gmail.com>
2023-02-27 21:09:48 +0100

    Added: - discount method - removeDiscount method - getCurrentValue method (that returns value) - changeValue method

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -2,13 +2,42 @@ package Model;
 public class Product {
     private String name;
     private String description;
-    private Float value;
-    private Boolean isOnSale;
+    private Float regularValue;
+    private Boolean isDiscounted;
+    private Float discountedValue;
 
-public Product(String name, String description, Float value) {
+    private Float currentValue;
+
+public Product(String name, String description, Float regularValue, Float discountedValue, Float currentValue) {
     this.name = name;
     this.description = description;
-    this.value = value;
-    this.isOnSale = false;
+    this.regularValue = regularValue;
+    this.discountedValue = discountedValue;
+    this.currentValue = currentValue;
+    this.isDiscounted = false;
+    }
+
+    public void discount(Integer percent) {
+    isDiscounted = true;
+    if(isDiscounted) {
+        discountedValue = regularValue - (percent * regularValue);
+        }
+    }
+
+    public void removeDiscount() {
+    isDiscounted = false;
+    discountedValue = 0f;
+    }
+
+    public float getCurrentValue() {
+        if(isDiscounted == false){
+            currentValue = regularValue;
+        }else if(isDiscounted) {
+            currentValue = discountedValue;
+        } return currentValue;
+    }
+
+    public void changeValue(Float newValue) {
+    regularValue = newValue;
     }
 }
