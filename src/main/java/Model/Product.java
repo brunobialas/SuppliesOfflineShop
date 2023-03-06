302cfb11aa00627a21d07f81466904c364000966
brunobialas <zehuwancode@gmail.com>
2023-03-06 17:48:05 +0100

    Task 15 done.

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -1,4 +1,5 @@
 package Model;
+
 public class Product {
     private String name;
     private String description;
@@ -6,26 +7,26 @@ public class Product {
     private Boolean isDiscounted;
     private Float discountedValue;
 
-public Product(String name, String description, Float regularValue, Float discountedValue) {
-    this.name = name;
-    this.description = description;
-    this.regularValue = regularValue;
-    this.discountedValue = discountedValue;
-    this.isDiscounted = false;
+    public Product(String name, String description, Float regularValue, Float discountedValue) {
+        this.name = name;
+        this.description = description;
+        this.regularValue = regularValue;
+        this.discountedValue = discountedValue;
+        this.isDiscounted = false;
     }
 
     public void discount(Integer percent) {
-    this.isDiscounted = true;
-    this.discountedValue = this.regularValue - (percent/100 * this.regularValue);
+        this.isDiscounted = true;
+        this.discountedValue = this.regularValue - (percent / 100 * this.regularValue);
     }
 
     public void removeDiscount() {
-    this.isDiscounted = false;
-    this.discountedValue = null;
+        this.isDiscounted = false;
+        this.discountedValue = null;
     }
 
     public float getCurrentValue() {
-        if(this.isDiscounted){
+        if (this.isDiscounted) {
             return this.discountedValue;
         } else {
             return this.regularValue;
