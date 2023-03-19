0146f20ac3d28631eab8e391a85767a73b5ee8a7
brunobialas <zehuwancode@gmai.com>
2023-03-19 17:06:11 +0100

    Task 17

---
+++ src/main/java/Model/ProductGenerator.java
@@ -0,0 +1,10 @@
+package Model;
+
+import java.util.Random;
+
+public class ProductGenerator {
+    private static String[] names = new String[3];
+    private static String[] descriptions = new String[3];
+}
+
+
